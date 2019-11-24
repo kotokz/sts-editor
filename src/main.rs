4a81e4ee9b404863ba2d140dd7f9def4dace43eb
kotokz <kotokz@live.com>
2019-11-24 23:02:39 +0800

    restructure

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-mod game_type;
-mod xor;
+mod sts_core;
 
 use anyhow::{Context, Result};
-use game_type::GameObj;
-use std::env;
-use std::fs;
-use std::str;
-use xor::Xor;
+use std::{env, str};
+use sts_core::{basic_edit, decrypt_save, encrypt_save};
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -37,55 +33,4 @@ fn find_savefile() -> Result<String> {
         .context("there is no matched file")??;
     let name = file.to_str().context("invalid filename")?;
     Ok(name.into())
-}
-
-fn basic_edit(game_map: &mut GameObj) -> &GameObj {
-    game_map.red += 1;
-    game_map.relics.push("Membership Card".to_string());
-    game_map.gold += 1000;
-    for card in game_map.cards.iter_mut() {
-        card.upgrades = 1;
-    }
-    game_map
-}
-
-fn decrypt_save(file_name: &str) -> Result<GameObj> {
-    let file = fs::read_to_string(file_name)?;
-    let raw_vec = base64::decode(&file).map(|raw| raw.xor(b"key"))?;
-    // println!("{:?}", str::from_utf8(&raw_vec));
-    let map: GameObj = serde_json::from_slice(&raw_vec)?;
-    Ok(map)
-}
-
-fn encrypt_save(file: &str, json: &GameObj) -> Result<()> {
-    let encrypted = serde_json::to_vec(json)
-        .and_then(|raw| Ok(raw.xor(b"key")))
-        .and_then(|ref raw| Ok(base64::encode(raw)))?;
-
-    fs::write(file, encrypted)?;
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
-    use super::*;
-
-    #[test]
-    fn test_decrypt() {
-        let game_map = decrypt_save("IRONCLAD.autosave").unwrap();
-
-        assert_eq!(game_map.cards[0].id, "Strike_R");
-        assert_eq!(game_map.relics.len(), 5);
-    }
-
-    #[test]
-    fn test_encrypt() {
-        let game_map = decrypt_save("WATCHER.autosave").unwrap();
-        encrypt_save("test2", &game_map).unwrap();
-
-        let test_json = decrypt_save("test2").unwrap();
-        assert_eq!(test_json.cards[0].id, "Defend_P");
-        assert_eq!(test_json.relics[1], "Membership Card");
-    }
 }
---
+++ src/sts_core/mod.rs
@@ -0,0 +1,58 @@
+mod game_type;
+mod xor;
+
+use anyhow::Result;
+use game_type::GameObj;
+use std::fs;
+use xor::Xor;
+
+pub fn basic_edit(game_map: &mut GameObj) -> &GameObj {
+    game_map.red += 1;
+    game_map.relics.push("Membership Card".to_string());
+    game_map.gold += 1000;
+    for card in game_map.cards.iter_mut() {
+        card.upgrades = 1;
+    }
+    game_map
+}
+
+pub fn decrypt_save(file_name: &str) -> Result<GameObj> {
+    let file = fs::read_to_string(file_name)?;
+    let raw_vec = base64::decode(&file).map(|raw| raw.xor(b"key"))?;
+    // println!("{:?}", str::from_utf8(&raw_vec));
+    let map: GameObj = serde_json::from_slice(&raw_vec)?;
+    Ok(map)
+}
+
+pub fn encrypt_save(file: &str, json: &GameObj) -> Result<()> {
+    let encrypted = serde_json::to_vec(json)
+        .and_then(|raw| Ok(raw.xor(b"key")))
+        .and_then(|ref raw| Ok(base64::encode(raw)))?;
+
+    fs::write(file, encrypted)?;
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_decrypt() {
+        let game_map = decrypt_save("IRONCLAD.autosave").unwrap();
+
+        assert_eq!(game_map.cards[0].id, "Strike_R");
+        assert_eq!(game_map.relics.len(), 5);
+    }
+
+    #[test]
+    fn test_encrypt() {
+        let game_map = decrypt_save("WATCHER.autosave").unwrap();
+        encrypt_save("test2", &game_map).unwrap();
+
+        let test_json = decrypt_save("test2").unwrap();
+        assert_eq!(test_json.cards[0].id, "Defend_P");
+        assert_eq!(test_json.relics[1], "Membership Card");
+    }
+}
